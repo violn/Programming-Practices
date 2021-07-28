00bf70be5e50ef17a99378e401e23334d6f13af4
violn <donedisontumaini@gmail.com>
2021-07-28 12:58:19 -0700

    Fixed some minor things with the code.

--- C#/HelloWorld.cs
+++ C#/HelloWorld.cs
@@ -1,10 +1,9 @@
 using System;
 
-
 public class HelloWorld
 {
     public static void main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        WriteLine("Hello World!");
     }
 }
--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.print("HelloWorld");
+		System.out.print("Hello World");
 	}
 }
